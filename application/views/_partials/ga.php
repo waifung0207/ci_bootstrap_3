426407285948092d60330ca6203455309b822f1e
Michael Chan <waifung0207@gmail.com>
2015-09-26 00:49:10 +0800

    Google Analytics can now be controlled from config/site.php alone

--- application/views/_partials/ga.php
+++ application/views/_partials/ga.php
@@ -1,10 +1,10 @@
+<?php if ( !empty($ga_id) ): ?>
 <script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	<?php // Update Google Analytics Tracker ID here ?>
-	ga('create', 'UA-XXXXXXXX-X', 'auto');
+	ga('create', '<?php echo $ga_id; ?>', 'auto');
 	ga('send', 'pageview');
 </script>
+<?php endif; ?>
