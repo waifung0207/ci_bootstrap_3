67b3b1c4f3e3435bc580836b75071f89fab3786a
Michael Chan <waifung0207@gmail.com>
2015-09-29 04:51:45 +0800

    Added Summernote as default texteditor for Grocery CRUD

--- application/modules/admin/config/grocery_crud.php
+++ application/modules/admin/config/grocery_crud.php
@@ -11,9 +11,9 @@
 	$config['grocery_crud_file_upload_allow_file_types'] 		= 'gif|jpeg|jpg|png|tiff|doc|docx|txt|odt|xls|xlsx|pdf|ppt|pptx|pps|ppsx|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|flv|avi|mpg|ogv|3gp|3g2';
 	$config['grocery_crud_file_upload_max_file_size'] 			= '20MB'; //ex. '10MB' (Mega Bytes), '1067KB' (Kilo Bytes), '5000B' (Bytes)
 
-	//You can choose 'ckeditor4', 'ckeditor', 'tinymce' or 'markitup'
-	$config['grocery_crud_default_text_editor'] = 'ckeditor4';
-	//You can choose 'minimal' or 'full'
+	//You can choose 'summernote', 'ckeditor4', 'ckeditor', 'tinymce' or 'markitup'
+	$config['grocery_crud_default_text_editor'] = 'summernote';
+	//You can choose 'minimal' or 'full' (only effective for 'ckeditor')
 	$config['grocery_crud_text_editor_type'] 	= 'full';
 
 	//The character limiter at the list page, zero(0) value if you don't want character limiter at your list page
--- assets/grocery_crud/js/jquery_plugins/config/jquery.ckeditor4.config.js
+++ assets/grocery_crud/js/jquery_plugins/config/jquery.ckeditor4.config.js
@@ -1,4 +1,5 @@
+// Added by CI Bootstrap 3
 $(function(){
-	$( 'textarea.texteditor' ).ckeditor({toolbar:'Full'});
+	$( 'textarea.texteditor' ).ckeditor({toolbar:'Full',width:700});
 	$( 'textarea.mini-texteditor' ).ckeditor({toolbar:'Basic',width:700});
 });
