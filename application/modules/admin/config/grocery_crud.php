68b1786aae9ccd8735d43e84676d878462f08736
Michael Chan <waifung0207@gmail.com>
2015-10-04 20:19:39 +0800

    Reverted Grocery CRUD default text editor to ckeditor

--- application/modules/admin/config/grocery_crud.php
+++ application/modules/admin/config/grocery_crud.php
@@ -12,7 +12,7 @@
 	$config['grocery_crud_file_upload_max_file_size'] 			= '20MB'; //ex. '10MB' (Mega Bytes), '1067KB' (Kilo Bytes), '5000B' (Bytes)
 
 	//You can choose 'summernote', 'ckeditor4', 'ckeditor', 'tinymce' or 'markitup'
-	$config['grocery_crud_default_text_editor'] = 'summernote';
+	$config['grocery_crud_default_text_editor'] = 'ckeditor';
 	//You can choose 'minimal' or 'full' (only effective for 'ckeditor')
 	$config['grocery_crud_text_editor_type'] 	= 'full';
 
