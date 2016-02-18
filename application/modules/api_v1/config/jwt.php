f5c328ac915e6035a0a44a0fc015b2ee61f9a6f9
Michael Chan <waifung0207@gmail.com>
2016-02-18 12:58:48 +0800

    Minor update

--- application/modules/api_v1/config/jwt.php
+++ application/modules/api_v1/config/jwt.php
@@ -9,9 +9,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['jwt_secret_key'] = 'example_key';
-
 $config['jwt_issuer'] = 'CI Bootstrap 3';
+
+// must be non-empty
+$config['jwt_secret_key'] = 'example_key';
 
 // expiry time since a JWT is issued (in seconds); set NULL to never expired
 $config['jwt_expiry'] = NULL;
