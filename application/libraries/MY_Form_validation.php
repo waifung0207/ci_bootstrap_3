6341f121e12e5c8abce29a9c2592e68a86bb42fd
Michael Chan <waifung0207@gmail.com>
2016-07-02 13:29:04 +0800

    Fixed MY_Form_validation bug

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -7,6 +7,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class MY_Form_validation extends CI_Form_validation {
 
 	public $CI;
+
+	public function __construct($rules = array())
+	{
+		parent::__construct($rules);
+		$this->CI =& get_instance();
+	}
 	
 	/**
 	 * Custom rules
