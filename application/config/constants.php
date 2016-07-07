a449edfd1ae20c95bbf6eb42b15f8c7a2181a3c6
Michael Chan <waifung0207@gmail.com>
2016-07-07 18:21:23 +0800

    Fix build number

--- application/config/constants.php
+++ application/config/constants.php
@@ -104,7 +104,7 @@ if (!(PHP_SAPI === 'cli' OR defined('STDIN')))
 }
 
 define('CI_BOOTSTRAP_REPO',			'https://github.com/waifung0207/ci_bootstrap_3');
-define('CI_BOOTSTRAP_VERSION',		'Build 20160706');	// will follow semantic version (e.g. v1.x.x) after first stable launch
+define('CI_BOOTSTRAP_VERSION',		'Build 20160707');	// will follow semantic version (e.g. v1.x.x) after first stable launch
 
 // Upload paths
 //define('UPLOAD_COVER_PHOTO',	'assets/uploads/cover_photos');
